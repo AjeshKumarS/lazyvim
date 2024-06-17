e39aed438bc2dbdc6095d69f2f260345870e1cc7
Ajesh Kumar S <ajeshkumar36020@gmail.com>
2024-06-17 19:10:11 +0530

    Add. word wrap config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,9 +5,17 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-vim.cmd("set clipboard=unnamed")
+vim.cmd("set clipboard=unnamedplus")
 vim.cmd("syntax on")
 vim.cmd("set number")
+vim.cmd("set formatoptions+=t")
 vim.g.mapleader = " "
+vim.opt.textwidth = 120
+vim.opt.colorcolumn = '120'
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.opt.breakindent = true
+
+
 
 -- }}}
--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -2,8 +2,12 @@ return {
   "catppuccin/nvim",
   lazy = false,
   name = "catppuccin",
+  opts = {
+    transparent_background = true,
+  },
   priority = 1000,
-  config = function()
+  config = function(_, opts)
+    require("catppuccin").setup(opts)
     vim.cmd.colorscheme("catppuccin")
   end,
 }
