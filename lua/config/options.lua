626dff060b148cc528e2e3b7a5511cff315304c9
Ajesh Kumar S <ajeshkumar36020@gmail.com>
2024-06-18 18:48:29 +0530

    Add. config for java

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,20 +2,18 @@
 
 vim.cmd("set nocompatible")
 vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
 vim.cmd("set clipboard=unnamedplus")
 vim.cmd("syntax on")
 vim.cmd("set number")
 vim.cmd("set formatoptions+=t")
 vim.g.mapleader = " "
 vim.opt.textwidth = 120
-vim.opt.colorcolumn = '120'
+vim.opt.colorcolumn = "120"
 vim.opt.wrap = true
 vim.opt.linebreak = true
 vim.opt.breakindent = true
-
-
 
 -- }}}
